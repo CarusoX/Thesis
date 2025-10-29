3ac12cb7355410e99721f90d4500ca4ed62a7616
carusox <uziludu@gmail.com>
2025-10-29 18:57:10 -0300

    chore: some testing

--- presentacion/src/scenes/example.tsx
+++ presentacion/src/scenes/example.tsx
@@ -1,12 +1,21 @@
-import {Circle, makeScene2D} from '@motion-canvas/2d';
+import {Layout, makeScene2D} from '@motion-canvas/2d';
 import {createRef} from '@motion-canvas/core';
+import { AnimatedArray } from '../components/AnimatedArray';
+
+const time = 0.5;
 
 export default makeScene2D(function* (view) {
-  // Create your animations here
+  const array = createRef<AnimatedArray>();
 
-  const circle = createRef<Circle>();
+  view.add(
+    <AnimatedArray ref={array} values={[1, 2, 3, 4, 5]} />
+  );
 
-  view.add(<Circle ref={circle} size={320} fill={'lightseagreen'} />);
-
-  yield* circle().scale(2, 2).to(1, 2);
+  yield* array().changeColor(0, 'red', time);
+  yield* array().changeColor(1, 'green', time);
+  yield* array().push(6, time);
+  yield* array().pop(time);
+  yield* array().push_front(0, time);
+  yield* array().pop_front(time);
+  yield* array().push_front(0, time);
 });
